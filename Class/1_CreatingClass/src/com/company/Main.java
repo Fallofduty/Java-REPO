d73ba95f4b434970d273fb8fe25a3d2461dbc527
Nafis <nafu22#gmail.com>
2020-09-16 16:15:57 -0400

    added class part 1

---
+++ Class/1_CreatingClass/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    var dispenser = new PezDisPenser();
+    /*
+    To make an object
+       use var or followed by class name
+         hence thiscase Pezdispenser(can also be var) dispencer the object name
+       dispenser = new PezDispenser(); // means new object has been created
+     */
+
+        System.out.println(dispenser.characterName); // Yoda
+        // yoda printed cause the default one was Yoda on the class
+        // thus this was printed
+
+        
+
+    }
+}
---
+++ Class/1_CreatingClass/src/com/company/PezDisPenser.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class PezDisPenser {
+    // naming conventions must be first letter must be upperCase and 2nd letter must be UpperCase  also
+
+    String characterName = "Yoda";
+
+}
